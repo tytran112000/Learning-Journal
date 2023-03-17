a64951276c619dd9bbb7b0a027c904d291499d9b
TungTran <62806679+tytran112000@users.noreply.github.com>
2023-03-16 22:47:50 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+export const blogs = [
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    {   
+        date: "JULY 23, 2022", 
+        name: "Blog one", 
+        img: "./images/blog-image-04.png",
+        description: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    },
+    
+]
